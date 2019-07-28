6fda48e4f65f9267ef0937fc4ebe40e01e39370b
nxlsBoy <nxls4605060315@gmail.com>
2019-07-28 21:50:57 +0800

    optimize step 2

--- chapter_1/_code/main.go
+++ chapter_1/_code/main.go
@@ -69,7 +69,7 @@ func (c *Customer) Statement() string {
 
 	for _, r := range c.Rentals {
 		// 计算单价
-		var thisAmount = c.AmountFor(r)
+		var thisAmount = r.AmountFor()
 
 		// 计算积分
 		points++
@@ -88,26 +88,6 @@ func (c *Customer) Statement() string {
 	expression += "Amount owed is " + strconv.Itoa(totalAmount) + "\n"
 	expression += "You earned " + strconv.Itoa(points) + " frequent renter points"
 	return expression
-}
-
-func (c *Customer) AmountFor(r *Rental) (thisAmount float32) {
-	switch r.Movie.PriceCode {
-	case Children:
-		thisAmount += 2
-		if r.DaysRented > 2 {
-			thisAmount += float32(r.DaysRented-2) * 1.5
-		}
-	case Regular:
-		thisAmount += float32(r.DaysRented) * 3
-	case NewRelease:
-		thisAmount += 1.5
-		if r.DaysRented > 3 {
-			thisAmount += float32(r.DaysRented-3) * 1.5
-		}
-	default:
-		log.Printf("error: not find MovieType%v", r.Movie.PriceCode)
-	}
-	return
 }
 
 type (
@@ -131,3 +111,24 @@ const (
 	Regular
 	NewRelease
 )
+
+// AmountFor 计算价格
+func (r *Rental) AmountFor() (thisAmount float32) {
+	switch r.Movie.PriceCode {
+	case Children:
+		thisAmount += 2
+		if r.DaysRented > 2 {
+			thisAmount += float32(r.DaysRented-2) * 1.5
+		}
+	case Regular:
+		thisAmount += float32(r.DaysRented) * 3
+	case NewRelease:
+		thisAmount += 1.5
+		if r.DaysRented > 3 {
+			thisAmount += float32(r.DaysRented-3) * 1.5
+		}
+	default:
+		log.Printf("error: not find MovieType%v", r.Movie.PriceCode)
+	}
+	return
+}
