56c9a5e18c18886c95a1539bc584b11067fdfa83
nxlsBoy <nxls4605060315@gmail.com>
2019-09-03 22:30:07 +0800

    1. move class - not good code

---
+++ move_class/main.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+手法：Move Class - 移动类
+
+目的：
+1. 细化职责
+*/
+
+func main() {
+
+}
+
+type Person struct {
+	Name                         string
+	OfficeAreaCode, OfficeNumber string
+}
